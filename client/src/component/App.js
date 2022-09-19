e95527dcc6bb5528adcdcb64740aabd005563fd8
GSCOM3902 <a3839660@gmail.com>
2022-09-19 16:24:17 +0800

    react build on heroku

--- client/src/component/App.js
+++ client/src/component/App.js
@@ -3,7 +3,7 @@ import React from "react";
 const App=()=>{
     return(
         <div>
-            connect!
+            connect to heroku!
         </div>
     );
 }
--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const app=express();
 
 const PORT=process.env.PORT||5000;
 
-app.get('/',(req,res)=>{
-    res.send("connenct!");
-})
+if(process.env.NODE_ENV==='production'){
+
+
+    app.use(express.static('client/build'));
+    //如果是前端要傳去第三方api，express認不得，就會執行bundle檔
+
+    const path=require("path");
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+    //如果route，express根本認不得，會直接產生html，通常是接收react路徑
+
+}
 
 app.listen(PORT);
