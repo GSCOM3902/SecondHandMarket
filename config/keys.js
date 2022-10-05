708577adc2c209dfe364f36a612b58aef9dc0725
GSCOM3902 <a3839660@gmail.com>
2022-10-06 04:32:09 +0800

    resovle conflick

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,5 @@
 module.exports={
-<<<<<<< HEAD
-    cookieKey:"ssdadad",
-    clientID:'481983099440-pjd39qjtbgns8pc2sspro2tu04k0amv7.apps.googleusercontent.com',
-    clientSecret:'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'
-=======
     clientID:'481983099440-pjd39qjtbgns8pc2sspro2tu04k0amv7.apps.googleusercontent.com',
     clientSecret:'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',
     cookieKey:'sdsdada'
->>>>>>> testBranch
 }
